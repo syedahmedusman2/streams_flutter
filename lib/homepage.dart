068986e8d878ba6eab19f0a70a0655cdec336d33
Ahmed Usman <ahmedusmanpakistan@gmail.com>
2023-07-31 01:17:17 +0500

    single and brooadcast stream

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
@@ -10,12 +12,41 @@ class StremExample extends StatefulWidget {
 }
 
 class _StremExampleState extends State<StremExample> {
+  StreamController<String> stringStream = StreamController();
+  late Stream<String> dataStream;
+  @override
+  void initState() {
+    // TODO: implement initState
+    dataStream = stringStream.stream.asBroadcastStream();
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Streams"),
-      ),
-    );
+        appBar: AppBar(
+          title: Text("Streams"),
+        ),
+        body: Column(
+          children: [
+            StreamBuilder<String>(
+              stream: dataStream,
+              builder: (context, snapshot) {
+                return Text(snapshot.data ?? "No data");
+              },
+            ),
+            TextField(
+              onChanged: (value) {
+                stringStream.add(value);
+              },
+            ),
+            StreamBuilder<String>(
+              stream: dataStream,
+              builder: (context, snapshot) {
+                return Text(snapshot.data ?? "No data");
+              },
+            ),
+          ],
+        ));
   }
 }
